a5f2b63fe47ebbca56266634ec416acfa740aedd
Gangadhar <gangadhardabbada@gmail.com>
2025-10-08 20:03:57 +0000

    convert all ts files into js files or delete that file of ts id not conv

--- src/ai/dev.ts
+++
@@ -1,6 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/ai-deficiency-detection.ts';
-import '@/ai/flows/ai-meal-analysis.ts';
-import '@/ai/flows/personalized-food-recommendations.ts';
